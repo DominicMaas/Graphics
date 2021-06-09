169aece5635c102c6e61c3ae0e783393d134e187
Dominic Maas <dominic.maas@live.com>
2021-06-09 19:13:12 +1200

    more improvements

--- projects/pixel_2d/src/chunk.rs
+++ projects/pixel_2d/src/chunk.rs
@@ -168,7 +168,6 @@ impl Chunk {
                     (((2.0 * gx * x_scale).sin() + (PI * gy * x_scale).sin()) * y_scale) + y_offset;
 
                 if r >= y as f32 {
-                    //e.g. n = 5
                     self.set_pixel_raw(x, y, Pixel::new(PixelType::Ground));
                 } else {
                     self.set_pixel_raw(x, y, Pixel::new(PixelType::Air));
@@ -180,7 +179,7 @@ impl Chunk {
     pub fn add_snow(&mut self) {
         for x in 0..CHUNK_SIZE {
             for y in 0..CHUNK_SIZE {
-                if self.rng.gen_range(0..100) > 95 {
+                if self.rng.gen_range(0..=100) > 95 {
                     let p = self.get_pixel_raw(x, y).unwrap().get_type();
                     match p {
                         PixelType::Air => {
@@ -218,7 +217,7 @@ impl Chunk {
 
     #[inline(always)]
     fn get_pixel_raw(&mut self, x: isize, y: isize) -> Option<&mut Pixel> {
-        if Self::pixel_in_bounds(x, y) == false {
+        if Self::pixel_in_bounds(Vector2::new(x, y)) == false {
             return None;
         }
 
@@ -227,7 +226,7 @@ impl Chunk {
 
     /// Grab a read only version of the pixel
     pub fn get_pixel(&self, x: isize, y: isize) -> Option<Pixel> {
-        if Self::pixel_in_bounds(x, y) == false {
+        if Self::pixel_in_bounds(Vector2::new(x, y)) == false {
             return None;
         }
 
@@ -236,7 +235,7 @@ impl Chunk {
 
     #[inline(always)]
     fn set_pixel_raw(&mut self, x: isize, y: isize, pixel: Pixel) {
-        if Self::pixel_in_bounds(x, y) == false {
+        if Self::pixel_in_bounds(Vector2::new(x, y)) == false {
             return;
         }
 
@@ -276,12 +275,13 @@ impl Chunk {
 
         for x in 0..CHUNK_SIZE {
             for y in 0..CHUNK_SIZE {
+                let pos = Vector2::new(x, y);
+
                 match self.get_pixel_raw(x, y) {
                     Some(pixel) => match pixel.get_type() {
-                        //PixelType::Ground => self.update_sand_old(x, y),
-                        PixelType::Water => self.update_water(x, y, dt),
-                        PixelType::Snow => self.update_sand_old(x, y),
-                        PixelType::Sand => self.update_sand(x, y, dt),
+                        PixelType::Water => self.update_water(pos, dt),
+                        PixelType::Snow => self.update_sand(pos, dt),
+                        PixelType::Sand => self.update_sand(pos, dt),
                         _ => {}
                     },
                     None => {}
@@ -290,47 +290,52 @@ impl Chunk {
         }
     }
 
-    fn update_sand(&mut self, x: isize, y: isize, dt: f32) {
-        let pos = Vector2::new(x, y);
-        let b_pos = Vector2::new(x, y - 1);
-        let br_pos = Vector2::new(x + 1, y - 1);
-        let bl_pos = Vector2::new(x - 1, y - 1);
+    fn update_sand(&mut self, pos: Vector2<isize>, dt: f32) {
+        let ran = self.rng.gen_range(0..=1);
+        let offset = if ran == 0 { 1 } else { -1 };
+
+        let b_pos = Vector2::new(pos.x, pos.y - 1);
+        let br_pos = Vector2::new(pos.x + offset, pos.y - 1);
+        let bl_pos = Vector2::new(pos.x + -offset, pos.y - 1);
 
         // Update the velocity
-        let pixel = self.get_pixel_raw(x, y).unwrap();
+        let pixel = self.get_pixel_raw(pos.x, pos.y).unwrap();
         pixel.velocity.y = f32::clamp(pixel.velocity.y + (GRAVITY * dt), -10.0, 10.0);
 
         // Just check if can move directly below, if not, then reset velocity
         if !self.pixel_is(b_pos, PixelType::Air) {
-            let pixel = self.get_pixel_raw(x, y).unwrap();
+            let pixel = self.get_pixel_raw(pos.x, pos.y).unwrap();
             pixel.velocity.y /= 2.0;
         }
 
-        let pixel = self.get_pixel(x, y).unwrap();
-        let v_pos = Vector2::new(x + pixel.velocity.x as isize, y + pixel.velocity.y as isize);
+        let pixel = self.get_pixel(pos.x, pos.y).unwrap();
+        let v_pos = Vector2::new(
+            pos.x + pixel.velocity.x as isize,
+            pos.y + pixel.velocity.y as isize,
+        );
 
         // Physics
         if self.pixel_is(v_pos, PixelType::Air) || (self.pixel_is(v_pos, PixelType::Water)) {
             if self.pixel_is(v_pos, PixelType::Air) {
-                self.swap_pixel_pos(pos, v_pos);
+                self.swap_pixel(pos, v_pos);
             }
         } else if self.pixel_is(b_pos, PixelType::Air) || self.pixel_is(b_pos, PixelType::Water) {
-            let pixel = self.get_pixel_raw(x, y).unwrap();
+            let pixel = self.get_pixel_raw(pos.x, pos.y).unwrap();
             pixel.velocity.y += GRAVITY * dt;
 
-            self.swap_pixel_pos(pos, b_pos);
+            self.swap_pixel(pos, b_pos);
         } else if self.pixel_is(bl_pos, PixelType::Air) || self.pixel_is(bl_pos, PixelType::Water) {
-            let pixel = self.get_pixel_raw(x, y).unwrap();
+            let pixel = self.get_pixel_raw(pos.x, pos.y).unwrap();
             pixel.velocity.x = 0.0;
             pixel.velocity.y += GRAVITY * dt;
 
-            self.swap_pixel_pos(pos, bl_pos);
+            self.swap_pixel(pos, bl_pos);
         } else if self.pixel_is(br_pos, PixelType::Air) || self.pixel_is(br_pos, PixelType::Water) {
-            let pixel = self.get_pixel_raw(x, y).unwrap();
+            let pixel = self.get_pixel_raw(pos.x, pos.y).unwrap();
             pixel.velocity.x = 0.0;
             pixel.velocity.y += GRAVITY * dt;
 
-            self.swap_pixel_pos(pos, br_pos);
+            self.swap_pixel(pos, br_pos);
         } else {
             // Do Nothing
         }
@@ -375,81 +380,103 @@ impl Chunk {
         // }
     }
 
-    fn update_sand_old(&mut self, x: isize, y: isize) {
-        if !self.pixel_at(x, y - 1) {
-            // If down empty
-            self.swap_pixel(x, y, x, y - 1);
-            return;
-        }
+    fn update_water(&mut self, pos: Vector2<isize>, dt: f32) {
+        let fall_rate = 2;
+        let spread_rate = 5;
 
-        // TODO: Better way of inverting
-        //if self.rng.gen_bool(0.5) {
-        if !self.pixel_at(x - 1, y - 1) {
-            // If down and left empty
-            self.swap_pixel(x, y, x - 1, y - 1);
-            return;
-        }
+        let ran = self.rng.gen_range(0..=1);
+        let spread_rate_right = if ran == 0 { spread_rate } else { -spread_rate };
+        let spread_rate_left = -spread_rate_right;
 
-        if !self.pixel_at(x + 1, y - 1) {
-            // If down and right empty
-            self.swap_pixel(x, y, x + 1, y - 1);
-            return;
-        }
-        //} else {
-        //    if !self.pixel_at(x + 1, y - 1) {
-        // If down and right empty
-        //        self.swap_pixel(x, y, x + 1, y - 1);
-        //        return;
-        //    }
-
-        //   if !self.pixel_at(x - 1, y - 1) {
-        // If down and left empty
-        //      self.swap_pixel(x, y, x - 1, y - 1);
-        //      return;
-        //  }
-        // }
-    }
-
-    fn update_water(&mut self, x: isize, y: isize, dt: f32) {
-        // We know that this pixel exists
-        let pixel = self.get_pixel_raw(x, y).unwrap();
-
-        let fall_rate: u32 = 2;
+        let b_pos = Vector2::new(pos.x, pos.y - fall_rate);
+        let br_pos = Vector2::new(pos.x + spread_rate_right, pos.y - fall_rate);
+        let bl_pos = Vector2::new(pos.x + spread_rate_left, pos.y - fall_rate);
+        let r_pos = Vector2::new(pos.x + spread_rate_right, pos.y);
+        let l_pos = Vector2::new(pos.x - spread_rate_left, pos.y);
 
         // Update the velocity
+        let pixel = self.get_pixel_raw(pos.x, pos.y).unwrap();
         pixel.velocity.y = f32::clamp(pixel.velocity.y + (GRAVITY * dt), -10.0, 10.0);
 
-        //let yv = f32::clamp(pixel_velocity.y + (GRAVITY * dt), -10.0, 10.0);
-        //pixel.set_velocity(Vector2::new(pixel_velocity.x, yv));
+        // Just check if can move directly below, if not, then reset velocity
+        if !self.pixel_is(b_pos, PixelType::Air) {
+            let pixel = self.get_pixel_raw(pos.x, pos.y).unwrap();
+            pixel.velocity.y /= 2.0;
+        }
 
-        if !self.pixel_at(x, y - 1) {
-            // If down empty
-            self.swap_pixel(x, y, x, y - 1);
-        } else if !self.pixel_at(x - 1, y - 1) {
-            // If down and left empty
-            self.swap_pixel(x, y, x - 1, y - 1);
-        } else if !self.pixel_at(x + 1, y - 1) {
-            // If down and right empty
-            self.swap_pixel(x, y, x + 1, y - 1);
-        } else if !self.pixel_at(x - 1, y) {
-            // If left empty
-            self.swap_pixel(x, y, x - 1, y);
-        } else if !self.pixel_at(x + 1, y) {
-            // If right empty
-            self.swap_pixel(x, y, x + 1, y);
+        let pixel = self.get_pixel(pos.x, pos.y).unwrap();
+        let v_pos = Vector2::new(
+            pos.x + pixel.velocity.x as isize,
+            pos.y + pixel.velocity.y as isize,
+        );
+
+        if self.pixel_is(v_pos, PixelType::Air) {
+            self.swap_pixel(pos, v_pos);
+        } else if self.pixel_is(b_pos, PixelType::Air) {
+            let pixel = self.get_pixel_raw(b_pos.x, b_pos.y).unwrap();
+            pixel.velocity.y += GRAVITY * dt;
+
+            self.swap_pixel(pos, b_pos);
+        } else if self.pixel_is(bl_pos, PixelType::Air) {
+            let x_vel = if let None = self.pixel_in_type(pos, PixelType::Water) {
+                if self.rng.gen_range(0..=1) == 0 {
+                    -1.0
+                } else {
+                    1.0
+                }
+            } else {
+                0.0
+            };
+
+            let pixel = self.get_pixel_raw(bl_pos.x, bl_pos.y).unwrap();
+            pixel.velocity.x = x_vel;
+            pixel.velocity.y += GRAVITY * dt;
+
+            self.swap_pixel(pos, bl_pos);
+        } else if self.pixel_is(br_pos, PixelType::Air) {
+            let x_vel = if let None = self.pixel_in_type(pos, PixelType::Water) {
+                if self.rng.gen_range(0..=1) == 0 {
+                    -1.0
+                } else {
+                    1.0
+                }
+            } else {
+                0.0
+            };
+
+            let pixel = self.get_pixel_raw(br_pos.x, br_pos.y).unwrap();
+            pixel.velocity.x = x_vel;
+            pixel.velocity.y += GRAVITY * dt;
+
+            self.swap_pixel(pos, br_pos);
+        } else if self.rng.gen_range(0..=10) == 0 {
+            // In water, sometimes move around?
+            if let Some(water_pos) = self.pixel_in_type(pos, PixelType::Water) {
+                self.swap_pixel(pos, water_pos);
+            }
+        } else {
+            // Don't try to spread if something directly above
+            if !self.pixel_completely_surrounded(pos) {
+                for i in 0..fall_rate {
+                    for j in (0..spread_rate).rev() {
+                        let x_minus_j_y_plus_i = Vector2::new(pos.x - j, pos.y + i);
+                        let x_plus_j_y_plus_i = Vector2::new(pos.x + j, pos.y + i);
+
+                        if self.pixel_is(x_minus_j_y_plus_i, PixelType::Air) {
+                            self.swap_pixel(pos, x_minus_j_y_plus_i);
+                        }
+
+                        if self.pixel_is(x_plus_j_y_plus_i, PixelType::Air) {
+                            self.swap_pixel(pos, x_plus_j_y_plus_i);
+                        }
+                    }
+                }
+            }
         }
     }
 
     /// Swap a pixel from "from" to "to"
-    pub fn swap_pixel(&mut self, from_x: isize, from_y: isize, to_x: isize, to_y: isize) {
-        let from = self.get_pixel_raw(from_x, from_y).unwrap().clone();
-        let to = self.get_pixel_raw(to_x, to_y).unwrap().clone();
-
-        self.set_pixel_raw(from_x, from_y, to);
-        self.set_pixel_raw(to_x, to_y, from);
-    }
-
-    pub fn swap_pixel_pos(&mut self, from: Vector2<isize>, to: Vector2<isize>) {
+    pub fn swap_pixel(&mut self, from: Vector2<isize>, to: Vector2<isize>) {
         let p_from = self.get_pixel_raw(from.x, from.y).unwrap().clone();
         let p_to = self.get_pixel_raw(to.x, to.y).unwrap().clone();
 
@@ -463,12 +490,6 @@ impl Chunk {
         self.set_pixel_raw(x, y, Pixel::new(pixel_type));
     }
 
-    // OLD: Returns true if there is a pixel at this area (not air)
-    fn pixel_at(&self, x: isize, y: isize) -> bool {
-        let is_air_or_out_of_bounds = self.pixel_is(Vector2::new(x, y), PixelType::Air);
-        return !is_air_or_out_of_bounds;
-    }
-
     fn pixel_is(&self, pos: Vector2<isize>, pixel_type: PixelType) -> bool {
         match self.get_pixel(pos.x, pos.y) {
             Some(pixel) => pixel.get_type() == pixel_type,
@@ -476,13 +497,115 @@ impl Chunk {
         }
     }
 
+    /// Returns none if the pixel is not in the given type, otherwise returns the
+    /// position in which the pixel is touching the type
+    fn pixel_in_type(&self, pos: Vector2<isize>, pixel_type: PixelType) -> Option<Vector2<isize>> {
+        if self.pixel_is(pos, pixel_type) {
+            return Some(pos);
+        }
+
+        if self.pixel_is(pos + Vector2::new(0, -1), pixel_type) {
+            return Some(pos + Vector2::new(0, -1));
+        }
+
+        if self.pixel_is(pos + Vector2::new(0, 1), pixel_type) {
+            return Some(pos + Vector2::new(0, 1));
+        }
+
+        if self.pixel_is(pos + Vector2::new(-1, 0), pixel_type) {
+            return Some(pos + Vector2::new(-1, 0));
+        }
+
+        if self.pixel_is(pos + Vector2::new(-1, -1), pixel_type) {
+            return Some(pos + Vector2::new(-1, -1));
+        }
+
+        if self.pixel_is(pos + Vector2::new(-1, 1), pixel_type) {
+            return Some(pos + Vector2::new(-1, 1));
+        }
+
+        if self.pixel_is(pos + Vector2::new(1, 0), pixel_type) {
+            return Some(pos + Vector2::new(1, 0));
+        }
+
+        if self.pixel_is(pos + Vector2::new(1, -1), pixel_type) {
+            return Some(pos + Vector2::new(1, -1));
+        }
+
+        if self.pixel_is(pos + Vector2::new(1, 1), pixel_type) {
+            return Some(pos + Vector2::new(1, 1));
+        }
+
+        return None;
+    }
+
+    fn pixel_completely_surrounded(&self, pos: Vector2<isize>) -> bool {
+        // Top
+        if Self::pixel_in_bounds(pos + Vector2::new(1, 0))
+            && self.pixel_is(pos + Vector2::new(1, 0), PixelType::Air)
+        {
+            return false;
+        }
+
+        // Bottom
+        if Self::pixel_in_bounds(pos + Vector2::new(-1, 0))
+            && self.pixel_is(pos + Vector2::new(-1, 0), PixelType::Air)
+        {
+            return false;
+        }
+
+        // Left
+        if Self::pixel_in_bounds(pos + Vector2::new(-1, 0))
+            && self.pixel_is(pos + Vector2::new(-1, 0), PixelType::Air)
+        {
+            return false;
+        }
+
+        // Right
+        if Self::pixel_in_bounds(pos + Vector2::new(1, 0))
+            && self.pixel_is(pos + Vector2::new(1, 0), PixelType::Air)
+        {
+            return false;
+        }
+
+        // Top Left
+        if Self::pixel_in_bounds(pos + Vector2::new(-1, -1))
+            && self.pixel_is(pos + Vector2::new(-1, -1), PixelType::Air)
+        {
+            return false;
+        }
+
+        // Top Right
+        if Self::pixel_in_bounds(pos + Vector2::new(1, -1))
+            && self.pixel_is(pos + Vector2::new(1, -1), PixelType::Air)
+        {
+            return false;
+        }
+
+        // Bottom Left
+        if Self::pixel_in_bounds(pos + Vector2::new(-1, 1))
+            && self.pixel_is(pos + Vector2::new(-1, 1), PixelType::Air)
+        {
+            return false;
+        }
+
+        // Bottom Right
+        if Self::pixel_in_bounds(pos + Vector2::new(1, 1))
+            && self.pixel_is(pos + Vector2::new(1, 1), PixelType::Air)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
     // ---------- Helpers ---------- //
     fn pixel_index(x: isize, y: isize) -> usize {
         CHUNK_SIZE as usize * x as usize + y as usize
     }
 
-    fn pixel_in_bounds(x: isize, y: isize) -> bool {
-        if x >= CHUNK_SIZE || y >= CHUNK_SIZE || x < 0 || y < 0 {
+    fn pixel_in_bounds(pos: Vector2<isize>) -> bool {
+        if pos.x >= CHUNK_SIZE || pos.y >= CHUNK_SIZE || pos.x < 0 || pos.y < 0 {
             return false;
         }
 
