e8b19a873c78717983080fb9de9f0c347230933a
Dominic Maas <dominic.maas@live.com>
2021-06-11 18:47:36 +1200

    basic setup

--- projects/titan/src/app.rs
+++ projects/titan/src/app.rs
@@ -1,16 +1,107 @@
-pub struct App {}
+use vesta::{
+    cgmath::num_traits::FloatConst,
+    winit::{
+        dpi::PhysicalSize,
+        event::{MouseButton, VirtualKeyCode},
+    },
+};
+
+pub struct App {
+    chunk_render_pipeline: vesta::wgpu::RenderPipeline,
+    camera: vesta::Camera,
+    camera_controller: vesta::CameraControllerTitan,
+}
 
 impl vesta::VestaApp for App {
-    fn init(_engine: &mut vesta::Engine) -> Self {
+    fn init(engine: &mut vesta::Engine) -> Self {
+        // Layout for shaders
+        let render_pipeline_layout =
+            engine
+                .renderer
+                .device
+                .create_pipeline_layout(&vesta::wgpu::PipelineLayoutDescriptor {
+                    label: Some("Render Pipeline Layout"),
+                    bind_group_layouts: &[
+                        &vesta::UniformBufferUtils::create_bind_group_layout(
+                            vesta::wgpu::ShaderStage::VERTEX,
+                            &engine.renderer.device,
+                        ),
+                        &vesta::UniformBufferUtils::create_bind_group_layout(
+                            vesta::wgpu::ShaderStage::VERTEX,
+                            &engine.renderer.device,
+                        ),
+                        &vesta::Texture::create_bind_group_layout(&engine.renderer.device),
+                    ],
+                    push_constant_ranges: &[],
+                });
+
+        // Render pipeline for shaders
+        let chunk_render_pipeline = vesta::RenderPipelineBuilder::new(
+            engine.renderer.swap_chain_desc.format,
+            "Chunk Render Pipeline",
+        )
+        .with_shader_source(vesta::wgpu::ShaderSource::Wgsl(
+            include_str!("res/chunk_shader.wgsl").into(),
+        ))
+        .with_layout(&render_pipeline_layout)
+        .build(&engine.renderer.device)
+        .unwrap();
+
+        // Setup the main camera
+        let camera = vesta::Camera::new(
+            (0.0, 0.0, 0.0).into(),
+            vesta::PerspectiveProjection::new(
+                engine.get_window_size().width,
+                engine.get_window_size().height,
+                vesta::cgmath::Rad(70.0 / 180.0 * f32::PI()),
+                0.01,
+                1000.0,
+            ),
+            &engine.renderer.device,
+        );
+
+        let camera_controller = vesta::CameraControllerTitan::new();
+
         // Init the engine
-        Self {}
+        Self {
+            chunk_render_pipeline,
+            camera,
+            camera_controller,
+        }
     }
 
     fn render<'a>(
         &'a mut self,
-        _render_pass: &mut vesta::wgpu::RenderPass<'a>,
+        render_pass: &mut vesta::wgpu::RenderPass<'a>,
         _engine: &vesta::Engine,
     ) {
-        // Perform rendering
+        render_pass.set_pipeline(&self.chunk_render_pipeline);
+        render_pass.set_bind_group(0, &self.camera.uniform_buffer.bind_group, &[]);
+    }
+
+    fn update(&mut self, engine: &mut vesta::Engine) {
+        self.camera_controller.process_input(
+            &mut self.camera,
+            &engine,
+            engine.is_cursor_captured(),
+        );
+        self.camera_controller.update_camera(&mut self.camera);
+
+        self.camera.update_uniforms(&engine.renderer);
+
+        // Add ability to escape out of camera
+        if engine.io.keyboard.get_key_down(VirtualKeyCode::Escape) && engine.is_cursor_captured() {
+            engine.set_cursor_captured(false);
+        }
+
+        // Add ability to capture camera again
+        if engine.io.mouse.get_button_down(MouseButton::Left) && !engine.is_cursor_captured() {
+            engine.set_cursor_captured(true);
+        }
+    }
+
+    fn resize(&mut self, size: PhysicalSize<u32>, _engine: &vesta::Engine) {
+        // The screen projection needs to be updated
+        self.camera.projection.resize(size.width, size.height);
     }
 }
---
+++ projects/titan/src/res/chunk_shader.wgsl
@@ -0,0 +1,56 @@
+// Vertex input and output
+struct VertexInput {
+    [[location(0)]] position: vec3<f32>;
+    [[location(1)]] color: vec3<f32>;
+    [[location(2)]] tex_coord: vec2<f32>;
+};
+
+struct VertexOutput {
+    [[builtin(position)]] position: vec4<f32>;
+    [[location(0)]] tex_coord: vec2<f32>;
+    [[location(1)]] normal: vec3<f32>;
+    [[location(2)]] vertex_position: vec3<f32>;  
+};
+
+// Data structures 
+[[block]]
+struct Camera {
+    view_proj: mat4x4<f32>;
+};
+
+[[block]]
+struct Model {
+    model: mat4x4<f32>;
+    normal: mat3x3<f32>;
+};
+
+// Uniform bindings
+[[group(0), binding(0)]]
+var<uniform> u_camera: Camera;
+
+[[group(1), binding(0)]]
+var<uniform> u_model: Model;
+
+// Vertex Shader
+[[stage(vertex)]]
+fn main(in: VertexInput) -> VertexOutput {
+    
+    var out: VertexOutput;
+    out.tex_coord = in.tex_coord;
+    out.position = u_camera.view_proj * u_model.model * vec4<f32>(in.position, 1.0); 
+    return out;
+}
+
+// ---------------------------------------------------------------- //
+
+[[group(2), binding(0)]]
+var u_diffuse_texture: texture_2d<f32>;
+
+[[group(2), binding(1)]]
+var u_sampler: sampler;
+
+// Fragment Shader
+[[stage(fragment)]]
+fn main(in: VertexOutput) -> [[location(0)]] vec4<f32> {
+    return textureSample(u_diffuse_texture, u_sampler, in.tex_coord);
+}
