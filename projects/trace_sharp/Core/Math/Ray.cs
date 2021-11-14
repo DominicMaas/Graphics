178408b6edfd01096a4bcfd040ea867cfdcfc7cd
Dominic Maas <dominic.maas@live.com>
2021-11-14 19:50:39 +1300

    parallel!

--- projects/trace_sharp/Core/Math/Ray.cs
+++ projects/trace_sharp/Core/Math/Ray.cs
@@ -4,21 +4,32 @@ namespace TraceSharp.Core.Math;
 
 public class Ray
 {
-    public Vector3 Origin { get; set; }
-    public Vector3 Direction { get; set; }
+    public Vector3 Origin { get; }
+    public Vector3 Direction { get; }
+
+    public Ray(Vector3 origin, Vector3 direction)
+    {
+        Origin = origin;
+        Direction = direction;
+    }
+
+    public Vector3 At(float t)
+    {
+        return Origin + Direction * t;
+    }
 
     public static Ray CreatePrime(int x, int y, Scene scene)
     {
         var fovAdjustment = MathF.Tan((scene.FieldOfView * (MathF.PI / 180.0f)) / 2);
         var aspectRatio = (float)scene.Width / (float)scene.Height;
 
-        var sensorX = ((((x + 0.5f) / scene.Width) * 2.0f - 1.0f) * aspectRatio) * fovAdjustment;
-        var sensorY = (1.0f - ((y + 0.5f) / scene.Height) * 2.0f) * fovAdjustment;
+        var sensorX = ((((x + 0.5f) / scene.Width) * 2.0f - 1.0f) * aspectRatio);
+        var sensorY = (1.0f - ((y + 0.5f) / scene.Height) * 2.0f);
 
-        return new Ray
-        {
-            Origin = new Vector3(),
-            Direction = Vector3.Normalize(new Vector3(sensorX, sensorY, -1.0f))
-        };
+        // Adjust for the FOV
+        sensorX *= fovAdjustment;
+        sensorY *= fovAdjustment;
+
+        return new Ray(new Vector3(),Vector3.Normalize(new Vector3(sensorX, sensorY, -1.0f)));
     }
 }
--- projects/trace_sharp/Program.cs
+++ projects/trace_sharp/Program.cs
@@ -6,13 +6,20 @@ using TraceSharp.Core;
 using TraceSharp.Core.Math;
 using TraceSharp.Core.Primitives;
 
+var JitterMatrix = new float[4 * 2] {
+    -1.0f/4.0f,  3.0f/4.0f,
+     3.0f/4.0f,  1.0f/3.0f,
+    -3.0f/4.0f, -1.0f/4.0f,
+     1.0f/4.0f, -3.0f/4.0f,
+};
+
 Vector3 GetColor(Scene scene, Ray ray, Intersection intersection)
 {
     var hitPoint = ray.Origin + (ray.Direction * intersection.Distance);
     var surfaceNormal = intersection.Object.SurfaceNormal(hitPoint);
     var directionToLight = -Vector3.Normalize(scene.Light.Direction);
 
-    var shadowRay = new Ray { Origin = hitPoint + (surfaceNormal * new Vector3(0.0001f)), Direction = directionToLight };
+    var shadowRay = new Ray(hitPoint + (surfaceNormal * new Vector3(0.0001f)), directionToLight);
     var inLight = scene.Trace(shadowRay) == null;
 
     var lightIntensity = inLight ? scene.Light.Intensity : 0.0f;
@@ -27,32 +34,48 @@ void Render(Scene scene)
 {
     using var image = new Image<Rgba32>(scene.Width, scene.Height);
 
-    for (int y = 0; y < scene.Height; y++)
+    // how many segments to split the image up to (how many threads)
+    const int HeightSegments = 16;
+    var HeightPerThread = scene.Height / HeightSegments;
+
+    void ProcessHeightSegment(int y0, int y1)
     {
-        var rowSpan = image.GetPixelRowSpan(y);
-
-        for (int x = 0; x < scene.Width; x++)
+        for (int y = y0; y < y1; y++)
         {
-            // Create ray
-            var ray = Ray.CreatePrime(x, y, scene);
-
-            var intersection = scene.Trace(ray);
-            if (intersection != null)
+            var rowSpan = image.GetPixelRowSpan(y);
+            for (int x = 0; x < scene.Width; x++)
             {
-                var color = GetColor(scene, ray, intersection);
+                var color = new Vector3(0, 0, 0);
+
+                // Create ray
+                var ray = Ray.CreatePrime(x, y, scene);
+
+                var intersection = scene.Trace(ray);
+                if (intersection != null)
+                {
+                    color = GetColor(scene, ray, intersection);
+                }
+
                 rowSpan[x] = new Rgba32(color.X, color.Y, color.Z);
             }
         }
     }
+
+    Parallel.For(0, HeightSegments, i => {
+        var y0 = HeightPerThread * i;
+        var y1 = (HeightPerThread * i) + HeightPerThread;
+
+        ProcessHeightSegment(y0, y1);
+    });
 
     image.SaveAsPng("output.png");
 }
 
 var myScene = new Scene
 {
-    Width = 3840,
-    Height = 2160,
-    FieldOfView = 60.0f,
+    Width = 1920,
+    Height = 1080,
+    FieldOfView = 75.0f,
     RenderObjects = new RenderObject[]
     {
         new Sphere { Position = new Vector3( 0.0f,  0.0f,  -5.0f), Radius = 1.0f, Color = new Vector3(1, 0, 0) },
