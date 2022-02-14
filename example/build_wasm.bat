44613b71f29c382e33d44e204de270bcc4c451d5
Dominic Maas <dominic.maas@live.com>
2022-02-15 11:38:12 +1300

    convert example to bevy

--- example/build_wasm.bat
+++
@@ -1,5 +0,0 @@
-SET RUSTFLAGS=--cfg=web_sys_unstable_apis 
-
-cargo build --target wasm32-unknown-unknown
-
-wasm-bindgen --out-dir web --web target/wasm32-unknown-unknown/debug/vesta_example.wasm
--- example/src/main.rs
+++ example/src/main.rs
@@ -1,16 +1,79 @@
-mod app;
-mod cube;
-
-use app::*;
-use vesta::winit::dpi::PhysicalSize;
+use bevy::prelude::*;
+use smooth_bevy_cameras::{
+    controllers::orbit::{OrbitCameraBundle, OrbitCameraController, OrbitCameraPlugin},
+    LookTransformPlugin,
+};
 
 fn main() {
-    // Config for the engine
-    let config = vesta::Config {
-        window_title: "Vesta Example".to_string(),
-        window_size: PhysicalSize::new(1920, 1080),
-    };
+    App::new()
+        .insert_resource(Msaa { samples: 4 })
+        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
+        .add_plugins(DefaultPlugins)
+        .add_plugin(LookTransformPlugin)
+        .add_plugin(OrbitCameraPlugin::default())
+        .add_startup_system(setup)
+        .run();
+}
 
-    // Create for App, and pass in the config
-    vesta::Engine::run::<App>(config);
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Texture for the cube
+    let texture_handle = asset_server.load("square.png");
+
+    // Simple plane that the cube is floating on top of
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 6.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        transform: Transform::from_xyz(0.0, -1.0, 0.0),
+        ..Default::default()
+    });
+
+    // Cube that we rotate around (also rotate around the plane)
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(StandardMaterial {
+            base_color_texture: Some(texture_handle.clone()),
+            alpha_mode: AlphaMode::Blend,
+            ..Default::default()
+        }),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        ..Default::default()
+    });
+
+    // Light for the scene
+    const HALF_SIZE: f32 = 10.0;
+    commands.spawn_bundle(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            // Configure the projection to better fit the scene
+            shadow_projection: OrthographicProjection {
+                left: -HALF_SIZE,
+                right: HALF_SIZE,
+                bottom: -HALF_SIZE,
+                top: HALF_SIZE,
+                near: -10.0 * HALF_SIZE,
+                far: 10.0 * HALF_SIZE,
+                ..Default::default()
+            },
+            shadows_enabled: true,
+            ..Default::default()
+        },
+        transform: Transform {
+            translation: Vec3::new(0.0, 2.0, 0.0),
+            rotation: Quat::from_rotation_x(-std::f32::consts::FRAC_PI_4),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+
+    // Main camera to look through
+    commands.spawn_bundle(OrbitCameraBundle::new(
+        OrbitCameraController::default(),
+        PerspectiveCameraBundle::default(),
+        Vec3::new(0.0, 0.0, 5.0),
+        Vec3::new(0., 0., 0.),
+    ));
 }
